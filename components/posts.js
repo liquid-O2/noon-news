4ca55e91b416ef88f1db57e1ec44360b3c0b8b09
A r u n a v a 雲 <this.is.arunava.b@gmail.com>
2022-09-20 22:20:11 +0530

    Changed font

--- components/posts.js
+++ components/posts.js
@@ -1,14 +1,14 @@
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link';
+import React from 'react';
 const options = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    "X-RapidAPI-Key": "eb94e4f8b7msh0ec80a5de6b6671p1d3c0cjsnbfb99a4eece1",
-    "X-RapidAPI-Host": "community-hacker-news-v1.p.rapidapi.com",
+    'X-RapidAPI-Key': 'eb94e4f8b7msh0ec80a5de6b6671p1d3c0cjsnbfb99a4eece1',
+    'X-RapidAPI-Host': 'community-hacker-news-v1.p.rapidapi.com',
   },
 };
 const idReducer = (state, action) => {
-  if (action.type === "success") {
+  if (action.type === 'success') {
     return {
       ...state,
       id: action.data.slice(0, 10),
@@ -17,7 +17,7 @@ const idReducer = (state, action) => {
 };
 
 const postReducer = (state, action) => {
-  if (action.type === "success") {
+  if (action.type === 'success') {
     return {
       ...state,
       title: [...state.title, action.data.title],
@@ -38,12 +38,12 @@ export function Posts() {
   });
   React.useEffect(() => {
     fetch(
-      "https://community-hacker-news-v1.p.rapidapi.com/topstories.json?print=pretty",
+      'https://community-hacker-news-v1.p.rapidapi.com/topstories.json?print=pretty',
       options
     )
       .then((response) => response.json())
       .then((response) => {
-        dispatchIds({ type: "success", data: response });
+        dispatchIds({ type: 'success', data: response });
       })
       .catch((err) => console.error(err));
   }, []);
@@ -56,7 +56,7 @@ export function Posts() {
         )
           .then((response) => response.json())
           .then((response) => {
-            dispatchPosts({ type: "success", data: response });
+            dispatchPosts({ type: 'success', data: response });
             console.log(response);
           })
           .catch((err) => console.error(err));
@@ -64,20 +64,26 @@ export function Posts() {
     }
   }, [ids.id]);
   return (
-    <div className="container mt-2">
-      {posts.id
-        ? posts.id.map((id, index) => (
-            <div key={id} className="card bg-dark-background mb-2">
-              <a href={posts.url[index]}>
-                <h2 className="font-md text-primary underline">
-                  {posts.title[index]}
-                </h2>
-              </a>
+    <div className='container mt-2'>
+      {posts.id ? (
+        posts.id.map((id, index) => (
+          <div key={id} className='card bg-dark-background mb-2'>
+            <a href={posts.url[index]}>
+              <h2 className='font-md grotesk text-primary underline'>
+                {posts.title[index]}
+              </h2>
+            </a>
 
-              <p className="font-sm o-80 mt-1">By : {posts.user[index]}</p>
-            </div>
-          ))
-        : null}
+            <p className='grotesk font-sm o-80 mt-1'>
+              By : {posts.user[index]}
+            </p>
+          </div>
+        ))
+      ) : (
+        <div className='container display-f justify-center font-lg'>
+          Loading...
+        </div>
+      )}
     </div>
   );
 }
