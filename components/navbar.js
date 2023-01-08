fb3253066c1b3c5fd6008f50a03eb2b2da4ad435
liquidO2 <this.is.arunava.b@gmail.com>
2023-01-08 10:53:10 +0530

    update: margin

--- components/navbar.js
+++ components/navbar.js
@@ -1,41 +1,33 @@
-import Link from 'next/link';
+import Link from 'next/link'
 
 const Navbar = ({ active }) => {
   return (
     <nav>
-      <div className='container display-f flex-column mt-3'>
+      <div className='container display-f flex-column mt-4'>
         <h3 className='mb-2 font-lg grotesk'>Noon News</h3>
         <div className='display-f'>
           {' '}
           <Link href='/'>
             {active === 'top' && active != 'new' ? (
-              <a className='font-md grotesk mr-1 text-white navhead'>
-                Top Stories
-              </a>
+              <a className='font-md grotesk mr-1 text-white navhead'>Top Stories</a>
             ) : (
-              <a className='font-md grotesk mr-1 text-primary underline navhead'>
-                Top Stories
-              </a>
+              <a className='font-md grotesk mr-1 text-primary underline navhead'>Top Stories</a>
             )}
           </Link>
           <Link href={'/new'}>
             {
               (active =
                 'new' && active != 'top' ? (
-                  <a className='font-md grotesk text-white navhead'>
-                    New Stories
-                  </a>
+                  <a className='font-md grotesk text-white navhead'>New Stories</a>
                 ) : (
-                  <a className='font-md grotesk text-primary underline navhead'>
-                    New Stories
-                  </a>
+                  <a className='font-md grotesk text-primary underline navhead'>New Stories</a>
                 ))
             }
           </Link>
         </div>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
